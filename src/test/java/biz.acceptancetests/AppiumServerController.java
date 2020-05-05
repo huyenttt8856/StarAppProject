14b4f64be46978fe9e4f02e6df2636b5bf2b5980
huyenttt8856 <64525157+huyenttt8856@users.noreply.github.com>
2020-05-05 09:00:39 +0700

    Add files via upload

---
+++ src/test/java/biz.acceptancetests/AppiumServerController.java
@@ -0,0 +1,32 @@
+package biz.common;
+
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
+import static io.appium.java_client.service.local.AppiumDriverLocalService.buildService;
+
+public class AppiumServerController {
+	private final static AppiumDriverLocalService service;
+	static {
+		System.out.println("Print Default Data");
+
+		service = buildService(new AppiumServiceBuilder().withIPAddress("0.0.0.0").usingPort(Integer.parseInt("4723")));
+	}
+
+	public static void startAppiumServer() {
+		try {
+			service.start();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void stopAppiumServer() {
+		try {
+			if (service.isRunning()) {
+				service.stop();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
